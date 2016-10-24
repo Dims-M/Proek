4b4cf5e34a83741ba7fe9c565432f5e0d6021750
Dmitriy <o.avto@i-cks.ru>
2016-10-24 18:28:10 +0300

    Первый коммит. Архив Прог                          По книге шилда                          Разбираем Свичь!!!!                          Архивный коммит                          Переделал угадайки в jar для запуска в bat нике                          угадайка символов в клаве
                             Начало прграммы рандом v.00002

--- src/hz/poKnigeShilda/Randomm.java
+++ src/hz/poKnigeShilda/Randomm.java
@@ -14,6 +14,12 @@ public class Randomm {
 
         int viborIgrokov, nachalo = 0;
 
+        int hranenie [] = new int[10];
+
+        String igrok1 = "Выбран игрок № 1";
+       String igrok2 = "Выбран игрок № 2";
+       String igrok3 = "Выбран игрок № 3";
+
        // Scanner scanner = new Scanner(System.in);
       //  viborIgrokov = scanner.nextInt();
         //Scanner scanner1 = new Scanner(System.in);
@@ -23,7 +29,7 @@ public class Randomm {
 
             System.out.println("Запустился рандомный метод");
 
-            System.out.println("Для выбора ингроков нажмите 1 для выхода 0");
+            System.out.println("Для выбора игроков нажмите 1 для выхода 0");
 
             Scanner scanner1 = new Scanner(System.in);
             nachalo = scanner1.nextInt();
@@ -36,22 +42,23 @@ public class Randomm {
                 switch (viborIgrokov) {
 
                     case 1: {
-                        System.out.println("Выбран игрок № 1");
+                        System.out.println(igrok1);
+
                         break;
                     }
 
                     case 2: {
-                        System.out.println("Выбран игрок № 2");
+                        System.out.println(igrok2);
                         break;
                     }
 
                     case 3: {
-                        System.out.println("Выбран игрок № 3");
+                        System.out.println(igrok3);
                         break;
                     }
 
                     default:
-                        System.out.println();
+                        System.out.println("Выберите игроков");
 
                 } //клнец свича
 
@@ -70,6 +77,16 @@ public class Randomm {
 
 
 
+    public void hranenieMassiva (int count){
+
+        int chetchik = count;
+
+        for (int i = 0; ; ){
+
+        }
+
+
+    }
 
 
 
