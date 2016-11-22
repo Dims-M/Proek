a3497997c2612314439ca9218f262bcb75c09755
Dmitriy <o.avto@i-cks.ru>
2016-11-22 13:35:46 +0200

    Первый коммит. Архив                                                                                 По книге шилда                                                                                 Разбираем Свичь!!!!                                                                                 Архивный коммит                                                                                 Переделал угадайки в jar для запуска в bat нике                                                                                 угадайка символов в клаве
                                                                                    Начало прграммы рандом v.00003
    
                                                                                    Новые методы в классе Car
                                                                                    Контрольный коммит+пуш
                                                                                    Добавлен класс для работы с методом finalize
                                                                                    Разбираем массивы  v_0.2
                                                                                    Разбираем пузерьковую сортировку
                                                                                    Разбор сортировки
                                                                                    Разбираем и  реализовываем класс на целендричискую очередь
                                                                                    Сортировка массивов с помощью встроеннного Класса Math

--- src/hz/poKnigeShilda/VsakoRaznoe2.java
+++ src/hz/poKnigeShilda/VsakoRaznoe2.java
@@ -6,12 +6,21 @@ package hz.poKnigeShilda;
 public class VsakoRaznoe2 {
 
 
-    public static void ocheredNe (){
-        System.out.println("Данный метод реализовывает не целендричискую очередь");
+   // public static void ocheredNe (){
+      String infoOClasse = "Данный метод реализовывает не целендричискую очередь";
+
+        char q []; // Массив для хранения данных
+        int putloc, getloc; // переменные для хранения индексов вставки и извлечения
+
+        VsakoRaznoe2 (int size){   // При создании обьекта указываем
+            q = new char[size+1];  // создаем массив с указанным размером
+            putloc = getloc = 0;    // присваиваем начальные значения
+        }
 
 
 
-    }// конец класса
+
+  //  }// конец класса
 
 
 
