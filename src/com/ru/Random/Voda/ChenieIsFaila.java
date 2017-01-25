6e1c835105de635e50964fbae9c5ae9598850883
Dmitriy <o.avto@i-cks.ru>
2017-01-25 13:53:05 +0300

    Первый коммит. Архив                                                                                 По книге шилда                                                                                 Разбираем Свичь!!!!                                                                                 Архивный коммит                                                                                 Переделал угадайки в jar для запуска в bat нике                                                                                 угадайка символов в клаве
                                                                                    Начало прграммы рандом v.00003
    
                                                                                    Новые методы в классе Car
                                                                                    Контрольный коммит+пуш
                                                                                    Добавлен класс для работы с методом finalize
                                                                                    Разбираем массивы  v_0.2
                                                                                    Разбираем пузерьковую сортировку
                                                                                    Разбор сортировки
                                                                                    Разбираем и  реализовываем класс на целендричискую очередь
                                                                                    Сортировка массивов с помощью встроеннного Класса Math

--- src/com/ru/Random/Voda/ChenieIsFaila.java
+++ src/com/ru/Random/Voda/ChenieIsFaila.java
@@ -10,34 +10,7 @@ public class ChenieIsFaila {
 
     public static void main(String[] args) {
 
-        System.out.println("Запись данных из файла");
-
-        Scanner scannerVvod = new Scanner(System.in);
-
-        System.out.println("Напишите что вам нужно.");
-
-        String vvodTexta =  scannerVvod.nextLine();
-
-      //  Так, в конструктор редается либо путь к файлу в виде строки, либо объект File, который ссылается на конкретный текстовый файл.
-       // Параметр append указывает, должны ли данные дозаписываться в конец файла (если параметр равен true),
-        //либо файл должен перезаписываться.
-
-        try(FileWriter writer = new FileWriter("C:\\tempLog.txt", true))
-        {
-            // запись всей строки
-            String text = vvodTexta;
-            writer.write(text);
-            // запись по символам
-            writer.append('\n');
-            writer.append('*');
-            writer.append('\n');
-            writer.flush();
-        }
-        catch(IOException ex){
-
-            System.out.println(ex.getMessage());
-        }
-
+        zapisFaila ();
 
 
 
@@ -49,23 +22,26 @@ public class ChenieIsFaila {
         System.out.println("Запись данных из файла");
 
         Scanner scannerVvod = new Scanner(System.in);
+        Scanner istok = new Scanner(System.in);
 
+        System.out.println("Введите адрес файла");
+        String istocnik = istok.nextLine();
         System.out.println("Напишите что вам нужно.");
-
         String vvodTexta =  scannerVvod.nextLine();
 
         //  Так, в конструктор редается либо путь к файлу в виде строки, либо объект File, который ссылается на конкретный текстовый файл.
         // Параметр append указывает, должны ли данные дозаписываться в конец файла (если параметр равен true),
         //либо файл должен перезаписываться.
-
-        try(FileWriter writer = new FileWriter("C:\\tempLog.txt", true))
+      //  C:\\tempFlog.txt
+       // try(FileWriter writer = new FileWriter("C:\\tempLog.txt", true))
+        try(FileWriter writer = new FileWriter(istocnik, true))
         {
             // запись всей строки
             String text = vvodTexta;
             writer.write(text);
             // запись по символам
             writer.append('\n');
-            writer.append('*');
+            writer.append(' ');
             writer.append('\n');
             writer.flush();
         }
