5dc61c7611ab559126a7e6c52a2411903b8fee3b
Dmitriy <o.avto@i-cks.ru>
2017-01-30 12:20:14 +0300

    Первый коммит. Архив                                                                                 По книге шилда                                                                                 Разбираем Свичь!!!!                                                                                 Архивный коммит                                                                                 Переделал угадайки в jar для запуска в bat нике                                                                                 угадайка символов в клаве
                                                                                    Начало прграммы рандом v.00003
    
                                                                                    Новые методы в классе Car
                                                                                    Контрольный коммит+пуш
                                                                                    Добавлен класс для работы с методом finalize
                                                                                    Разбираем массивы  v_0.2
                                                                                    Разбираем пузерьковую сортировку
                                                                                    Разбор сортировки
                                                                                    Разбираем и  реализовываем класс на целендричискую очередь
                                                                                    Сортировка массивов с помощью встроеннного Класса Math

--- src/com/ru/Random/Voda/ChenieIsFaila.java
+++ src/com/ru/Random/Voda/ChenieIsFaila.java
@@ -1,6 +1,8 @@
 package com.ru.Random.Voda;
 import java.io.*;
 import java.io.FileWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Scanner;
 
 /**
@@ -10,8 +12,9 @@ public class ChenieIsFaila {
 
     public static void main(String[] args) {
 
-        zapisFaila ();
-
+        //zapisFaila ();
+       // chenoeFailov ();
+        zapisDateV_Fail ();
 
 
     }  // конец майла
@@ -29,6 +32,10 @@ public class ChenieIsFaila {
         System.out.println("Напишите что вам нужно.");
         String vvodTexta =  scannerVvod.nextLine();
 
+        Date currentDate = new Date();
+        SimpleDateFormat dateFormat =
+                new SimpleDateFormat("'Сегодняшная Дата: 'E dd.MM.yyyy'\nВремя: ' hh:mm:ss");
+
         //  Так, в конструктор редается либо путь к файлу в виде строки, либо объект File, который ссылается на конкретный текстовый файл.
         // Параметр append указывает, должны ли данные дозаписываться в конец файла (если параметр равен true),
         //либо файл должен перезаписываться.
@@ -41,7 +48,11 @@ public class ChenieIsFaila {
             writer.write(text);
             // запись по символам
             writer.append('\n');
-            writer.append(' ');
+           // writer.append(currentDate.toString());
+           //writer.append('\n');
+            // ввод даты
+            writer.append(dateFormat.format(currentDate));
+            System.out.println("Данные записаны...");
             writer.append('\n');
             writer.flush();
         }
@@ -50,6 +61,64 @@ public class ChenieIsFaila {
             System.out.println(ex.getMessage());
         }
     }
+
+    public static void  chenoeFailov (){
+        System.out.println("Чтение данных из файла.");
+
+        try(FileReader reader = new FileReader("C:\\tempLogChetieFaila.txt"))
+        { // Используем для чтениея класс FileReader. В конструктор передаем адрес хранеия файла.
+            // читаем посимвольно
+            int c;
+            while((c = reader.read())!=-1){
+
+                System.out.print((char)c);
+            }
+        }
+        catch(IOException ex){
+
+            System.out.println(ex.getMessage());
+        }
+
+    } // конец метода чтения файла
+
+
+    public static void zapisDateV_Fail () {
+        System.out.println("Запись даты в измененный файл");
+
+        System.out.println("Введите адрес файла в который требуется ввести изменения даты ");
+        Scanner scanner = new Scanner(System.in);
+        String adresFaila = scanner.nextLine();
+
+        Date currentDate = new Date();
+        // Создаем обьект для работы с Датой
+        SimpleDateFormat dateFormat =
+                new SimpleDateFormat("'Измененно: 'E dd.MM.yyyy'\nВремя: ' hh:mm:ss");
+        // форматированная форма вывода.
+        try(FileWriter writer = new FileWriter(adresFaila, true))
+        {
+            writer.append(dateFormat.format(currentDate));
+            System.out.println("Данные записаны...");
+            //writer.append('\n');
+            writer.flush();
+        }
+        catch(IOException ex){
+
+            System.out.println(ex.getMessage());
+        }
+
+
+    }
+
+
+
+    public static void sozdanieI_ZapisFaila (){
+        System.out.println("Создание и запись файла");
+
+
+
+    }
+
+
 
 
 }// конец класса
