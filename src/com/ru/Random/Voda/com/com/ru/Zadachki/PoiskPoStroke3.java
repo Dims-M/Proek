2aaabdf752e3d7d1d8fbaf7a350d9ddd2ab7fbf5
Dmitriy <o.avto@i-cks.ru>
2017-02-02 14:13:03 +0300

    Первый коммит. Архив                                                                                 По книге шилда                                                                                 Разбираем Свичь!!!!                                                                                 Архивный коммит                                                                                 Переделал угадайки в jar для запуска в bat нике                                                                                 угадайка символов в клаве
                                                                                    Начало прграммы рандом v.00003
    
                                                                                    Новые методы в классе Car
                                                                                    Контрольный коммит+пуш
                                                                                    Добавлен класс для работы с методом finalize
                                                                                    Разбираем массивы  v_0.2
                                                                                    Разбираем пузерьковую сортировку
                                                                                    Разбор сортировки
                                                                                    Разбираем и  реализовываем класс на целендричискую очередь
                                                                                    Сортировка массивов с помощью встроеннного Класса Math

--- src/com/ru/Random/Voda/com/com/ru/Zadachki/PoiskPoStroke3.java
+++ src/com/ru/Random/Voda/com/com/ru/Zadachki/PoiskPoStroke3.java
@@ -1,5 +1,8 @@
 package com.ru.Random.Voda.com.com.ru.Zadachki;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * Created by Администратор on 02.02.2017.
  */
@@ -9,7 +12,8 @@ public class PoiskPoStroke3 {
             "Регулярные выражения представляют мощный инструмент для обработки строк, который может использоваться в дополнение к методам класса String. " +
             "В java функциональность регулярных выражений сосредоточена в пакете java.util.regex.";
 
-static String choIchem = " ";
+    static String choIchem = "java";
+
     public static void main(String[] args) {
 
         poiskPoStroke (choIchem, istochnuk);
@@ -18,13 +22,33 @@ static String choIchem = " ";
 
     public static void poiskPoStroke (String poisk, String istochnukPoiska){
 
-
+        String adresssSohranenieIstorii = "C:\\zadachki\\rezultatiPoiskaPoStroke";
         System.out.println("Производим поиск по тексту...");
 
+        // создаем обьект pattern класа Pattern для работы с шаблоном поиска.
+        // метод Pattern.compile("Что ищем"); Статического метода compile(), который позволяет установить нужный нам шаблон.
+        // compile("Что ищем") В параметрах указываем что мы ищем.
+        Pattern pattern = Pattern.compile(poisk);
 
+        // Создаем обьектную переменную matcher класса Matcher для работы с источником поиска.
+        Matcher matcher =  pattern.matcher(istochnukPoiska); // В качестве параметра  pattern.matcher(istochnukPoiska) указываем где нам производить поиск
 
+        // В булевскую переменную указываем истина если поиск завершился успешно.
+        boolean proverka = matcher.matches();
 
-    }
+        if (proverka){
+            System.out.println("Поиск завершен");
+            String tempStroka = "Вы искали "+ poisk;
+            System.out.println(tempStroka);
+
+            ChenieIz_Faila2 chenieIz_faila2 = new ChenieIz_Faila2();
+            chenieIz_faila2.zapicTextovogoFaila(tempStroka,adresssSohranenieIstorii);
+        }
+        else {
+            System.out.println("Поиск не удачный...Попробуйте изменить запрос");
+        }
+
+    } // конец метода поиск
 
 
 }
