a438ebb04fb85fc7e5ec728b074c5f9210405af1
Dmitriy <o.avto@i-cks.ru>
2017-02-03 17:53:29 +0300

    Первый коммит. Архив                                                                                 По книге шилда                                                                                 Разбираем Свичь!!!!                                                                                 Архивный коммит                                                                                 Переделал угадайки в jar для запуска в bat нике                                                                                 угадайка символов в клаве
                                                                                    Начало прграммы рандом v.00003
    
                                                                                    Новые методы в классе Car
                                                                                    Контрольный коммит+пуш
                                                                                    Добавлен класс для работы с методом finalize
                                                                                    Разбираем массивы  v_0.2
                                                                                    Разбираем пузерьковую сортировку
                                                                                    Разбор сортировки
                                                                                    Разбираем и  реализовываем класс на целендричискую очередь
                                                                                    Сортировка массивов с помощью встроеннного Класса Math

--- src/com/ru/Random/Voda/com/com/ru/Zadachki/vsakoRaznoeMetodi.java
+++ src/com/ru/Random/Voda/com/com/ru/Zadachki/vsakoRaznoeMetodi.java
@@ -1,14 +1,19 @@
 package com.ru.Random.Voda.com.com.ru.Zadachki;
 
-/**
- * Created by Администратор on 03.02.2017.
- */
+import java.util.Random;
+
+
 public class vsakoRaznoeMetodi {
 
 
     public static void main(String[] args) {
 
+        //System.out.println(SlozenieChisel(100,500));
+       // System.out.println( MaxIMinimumChislo(SlozenieChisel(100,500), 500));
 
+        //System.out.println( vivodMassivovInt(random_Massiva(sosdanieMassiva(10))));
+
+        System.out.println( slozenieMassivah(vivodMassivovInt(random_Massiva(sosdanieMassiva(100))))) ;
 
     }
 
@@ -41,4 +46,55 @@ public class vsakoRaznoeMetodi {
 
     }
 
-}
+
+    public static int [] random_Massiva ( int massiv []) {
+        System.out.println("Рандомное заполнение массива");
+
+
+
+        for (int i = 0; i < massiv.length; i++) {
+            Random myRandoMassiva = new Random();
+
+            int tempMassiv = myRandoMassiva.nextInt(100);
+
+            // Random myRandoMassiva = new Random();
+
+            //int tempMassiv = myRandoMassiva.nextInt(1000);
+            massiv[i] = tempMassiv;
+           // System.out.println(massiv[i]);
+
+        }
+        return massiv;
+    }
+
+    public static void vivodMassivovInt (int massiv []){
+
+        System.out.println("Вывод содержимого массива");
+
+        for (int tempMassiva : massiv){
+            System.out.println(tempMassiva);
+        }
+       // return "Выполнено";
+    }
+
+
+    public static int[] sosdanieMassiva (int dlina){
+
+        int [] massim = new int[dlina];
+
+        return massim;
+    }
+
+    public static int slozenieMassivah (int mas[]) {
+
+        int rezultat = 0;
+
+        for (int temp : mas) {
+
+            rezultat += temp;
+        }
+        return rezultat;
+    }
+
+
+} // конец класса
