7727e6fac73dd1ac72168e91381572422f2c3fab
Dmitriy <o.avto@i-cks.ru>
2017-02-21 13:56:59 +0300

    Первый коммит. Архив                                                                                 По книге шилда                                                                                 Разбираем Свичь!!!!                                                                                 Архивный коммит                                                                                 Переделал угадайки в jar для запуска в bat нике                                                                                 угадайка символов в клаве
                                                                                    Начало прграммы рандом v.00003
    
                                                                                    Новые методы в классе Car
                                                                                    Контрольный коммит+пуш
                                                                                    Добавлен класс для работы с методом finalize
                                                                                    Разбираем массивы  v_0.2
                                                                                    Разбираем пузерьковую сортировку
                                                                                    Разбор сортировки
                                                                                    Разбираем и  реализовываем класс на целендричискую очередь
                                                                                    Сортировка массивов с помощью встроеннного Класса Math

--- src/com/ru/Random/Voda/com/com/ru/Zadachki/MyCollection.java
+++ src/com/ru/Random/Voda/com/com/ru/Zadachki/MyCollection.java
@@ -1,6 +1,9 @@
 package com.ru.Random.Voda.com.com.ru.Zadachki;
 
 import java.io.*;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
 
 /**
  * Created by Администратор on 17.02.2017.
@@ -11,7 +14,8 @@ public class MyCollection {
 
     public static void main(String[] args) {
 
-        chenieFailof(istochnik);
+       // chenieFailof(istochnik);
+        zapis_I_Sozdanie_Faila ();
 
     }
 
@@ -20,6 +24,22 @@ public class MyCollection {
         // чтение из файлов
 
         try {
+
+            // создаем обьект для Работы с датой.
+            // Для Вставки даты в лог
+
+            // 1) создали обьекта для работы с датой
+            Date myDate = new Date();
+
+            //2) Создаем обьет для форматирования и вывода даты. В параметра задаем  формат нужного вывода.
+            SimpleDateFormat myFormatDaty = new SimpleDateFormat("'ДАТА: 'E dd.MM.yyyy' Time: ' hh:mm:ss \n");
+
+            //3) Выводим просто дату
+            String tekushaiaData = myDate.toString();
+            //4) Вывод отформатированую дату
+            String tekushaiaFOrmatData = myFormatDaty.format(myDate);
+            // System.out.println("Formatted current date:\n" + dateFormat.format(currentDate));
+
             // создаем объект для чтения файла
 
             //использовать для записи байтов в файл
@@ -38,16 +58,18 @@ public class MyCollection {
             //для вывода в консоль присваиваим в стринг
             //Создаем обьект типа стринг и в качестве параметров указываем массив data
             String text = new String(data);
+            String Formattext = new String(data);
 
             System.out.println(text);
 
-            String info1 = "Содержимое файла "+istochnik;
-            String info2 = "Длина битового массива "+ data.length;
-            String info3 = "********************";
+            String info1 = "Содержимое файла "+istochnik+"\n";
+            String info2 = "Длина битового массива "+ data.length +"\n";
+            String info3 = "******************** \n";
 
             System.out.println(info1);
             System.out.println(info2);
-            System.out.println("info3");
+            System.out.println(myDate);
+            System.out.println(info3);
             //FileReader reader = new FileReader(String.valueOf(stream));
 
             //*/*********************************************************************
@@ -59,10 +81,17 @@ public class MyCollection {
             writer.write(info1);
             writer.write(info2);
             writer.write(info3);
+          //  writer.write(tekushaiaData);
+            writer.write(tekushaiaFOrmatData);
             writer.write(text);
-
-
+           // writer.write(tekushaiaData);
+           // writer.write(tekushaiaFOrmatData);
+            //writer.write(text);
+           // writer.write(tekushaiaData);
+           // writer.write(tekushaiaFOrmatData);
             // **********************************
+
+
 
         } catch (FileNotFoundException e) {
             System.out.println("Ошибка чтения при открытии файла   ");
@@ -73,5 +102,59 @@ public class MyCollection {
         }
 
     }
+
+
+    public static void zapis_I_Sozdanie_Faila()  {
+
+        try {
+
+        System.out.println("Введите имя сохранения");
+        Scanner scanner1 = new Scanner(System.in);
+        String nameFile1 = scanner1.nextLine();
+
+        System.out.println("Введите нужный текст и нажмите ЕНТР");
+        Scanner scanner = new Scanner(System.in);
+        String tekst = scanner.nextLine();
+
+
+         String failIstochnik = "C:\\zadachki\\"+nameFile1+".txt";
+
+            // Создали обьект для записи файла
+            File myFale = new File(nameFile1);
+
+            // проверяем. Если файл с таким именем не существует то создаем новый файл
+            if (!myFale.exists()) {
+                myFale.createNewFile();
+            }
+
+            // Работа с датой
+            Date myData = new Date();
+            // создаем обьект для форматирования даны(с помощью патерна(шаблона))
+            SimpleDateFormat simpleFormatData = new SimpleDateFormat("'ДАТА Cоздания: 'E dd.MM.yyyy' Time: ' hh:mm:ss \n");
+            String vivodDate = simpleFormatData.format(myData);
+
+            // Создаем обьект для записи файла в текс. Указываем путь записи
+            FileWriter myWriter = new FileWriter(failIstochnik,false);
+
+            // Запись текста в файл
+            myWriter.write(tekst);
+            myWriter.write(vivodDate);
+
+
+        } // конец экшепшена
+
+    catch (FileNotFoundException e) {
+        System.out.println("Ошибка чтения при открытии файла   ");
+        e.printStackTrace();
+    } catch (IOException e) {
+        System.out.println("Ошибка определения длины потока ");
+        e.printStackTrace();
+    }
+
+        finally {
+           System.out.println("Закрытия программы....");
+        }
+
+    } // конец метода
 
 }
